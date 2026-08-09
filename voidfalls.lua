12ce372d2692fa2afe8f26540d15725249fa292c
kfclover67 <kamilpc2012@gmail.com>
2026-08-09 03:13:25 +0200

    Create voidfalls.lua

---
+++ voidfalls.lua
@@ -0,0 +1,3 @@
+-- i see u wanna skid wow
+script_key = "trial"
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/0c9baaeef92468f4ff867e16c20c0baa2abc3201b688d1bae11354322c2a3ce3.lua"))()

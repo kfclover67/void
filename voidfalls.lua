100b04dbb350296cb13261304939d6c3774fbc25
kfclover67 <kamilpc2012@gmail.com>
2026-08-09 03:12:59 +0200

    Delete voidfalls.lua

--- voidfalls.lua
+++
@@ -1,3 +0,0 @@
--- tell me why u wanna skid 
-script_key = "trial"
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/0c9baaeef92468f4ff867e16c20c0baa2abc3201b688d1bae11354322c2a3ce3.lua"))()
